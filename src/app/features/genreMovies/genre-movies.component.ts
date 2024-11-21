96b7906d500865288a44be7ff48595b8aa533e9e
vquintero09 <quinterodev_09@outlook.com>
2024-11-21 16:45:26 -0700

    Add functionality to view movies by genre

--- src/app/features/genreMovies/genre-movies.component.ts
+++ src/app/features/genreMovies/genre-movies.component.ts
@@ -1,24 +1,32 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Imovies } from '@core/interfaces/movies-api.interface';
 import { MoviesApiService } from '@core/services/movies-api.service';
 import { MovieCardComponent } from '@shared/components/movie-card/movie-card.component';
 import { Observable } from 'rxjs';
+import { IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {arrowBackOutline} from 'ionicons/icons'
 
 @Component({
   selector: 'app-category',
   templateUrl: './genre-movies.component.html',
   styleUrl: './genre-movies.component.scss',
   standalone: true,
-  imports: [ AsyncPipe, MovieCardComponent ]
+  imports: [ AsyncPipe, MovieCardComponent, IonIcon ]
 })
 export class GenreMoviesComponent implements OnInit {
   private readonly _route = inject(ActivatedRoute);
   private readonly _moviesApiService = inject(MoviesApiService);
+  private readonly _router = inject(Router)
 
   genre!: string | null;
   movies$!: Observable<Imovies[]>;
+
+  constructor(){
+    addIcons({arrowBackOutline})
+  }
 
   ngOnInit(): void {
     this.genre = this._route.snapshot.paramMap.get('genre');
@@ -30,5 +38,9 @@ export class GenreMoviesComponent implements OnInit {
 
   getMoviesByGenre(genre: string){
     this.movies$ = this._moviesApiService.getMoviesByGenre(genre);
+  }
+
+  backHome(){
+    this._router.navigateByUrl("/")
   }
 }
