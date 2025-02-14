d9e9a3476a96bc3c273b5700ac0ab5672b883827
vquintero09 <quinterodev_09@outlook.com>
2025-02-13 17:44:27 -0700

    minor changes

--- src/app/core/interfaces/movies-api.interface.ts
+++ src/app/core/interfaces/movies-api.interface.ts
@@ -1,16 +1,35 @@
-export type Genres = "action" | "adventure" | "sci-Fi" | "drama" | "romance" | "biography" | "fantasy" | "anime";
+// export type Genres = "Accion" | "Aventura" | "Ciencia Ficción" | "Drama" | "Romance" | "biography" | "Fantasía" | "Anime" | "infantil";
+export type Plataforms = "Netflix" | "Max" | "Amazon Prime Video" | "Disney+" | "Paramount+" | "Apple TV+" | "Crunchyroll";
+
+export enum Genres  {
+  Accion = 'Action',
+  Aventura = 'Adventure',
+  "Ciencia Ficcion" = 'Sci-Fi',
+  Drama = 'Drama',
+  Romance = 'Romance',
+  Biografia = 'Biografia',
+  Fantasia = 'Fantasy',
+  Anime = 'Anime',
+  Crimen = 'Crime',
+  Terror = 'Terror',
+  Infantil = 'Infantil',
+  Suspenso = 'Suspenso'
+}
 
 
 export interface Imovies {
   id: string,
   title: string;
-  year: number;
+  year: number | null;
   director: string;
-  duration: number;
+  duration: number | null;
   poster: string;
   rate: number;
   image?: string;
   genre: Genres[];
   description?:string;
-  estreno?: string;
-}
+  plataforms?: Plataforms[];
+};
+
+export type newMovieEntry = Omit<Imovies, "id">;
+
--- src/app/core/services/urls-api.ts
+++ src/app/core/services/urls-api.ts
@@ -1,4 +1,9 @@
-const url = 'http://localhost:3000/auth';
-export const URL_AUTH_LOGIN = `${url}/login`;
-export const URL_CREATE = `${url}/register`;
-export const URL_AUTH_REFRESH = `${url}/refresh-token`
+const url = 'http://localhost:3000';
+
+export const URL_AUTH_LOGIN = `${url}/auth/login`;
+export const URL_CREATE = `${url}/auth/register`;
+export const URL_AUTH_REFRESH = `${url}/auth/refresh-token`;
+
+export const URL_GET_ALL_MOVIES = `${url}/movies`;
+export const URL_GET_MOVIE_BY_ID = `${url}/movies/:id`;
+export const URL_CREATE_MOVIE = `${url}/movies/createMovie`;
